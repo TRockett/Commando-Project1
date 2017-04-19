836de343250868d817c6a04b477136d3151663f5
ninalb13 <nina.lb.x@gmail.com>
2017-04-19 22:33:28 +0200

    WTF

--- ModuleHelicopter.h
+++ ModuleHelicopter.h
@@ -1,29 +1,29 @@
-#pragma once
-#include "Module.h"
-
-struct SDL_Texture;
-
-class ModuleHelicopter :
-	public Module
-{
-
-public:
-	SDL_Texture* sprites = nullptr;
-	Animation* current_animation = nullptr;
-
-	
-	SDL_Texture* helicopter_1;
-	SDL_Texture* helicopter_2;
-	SDL_Texture* helicopter_3;
-
-	Animation helicopter1;
-	Animation helix1;
-	Animation helix2;
-	ModuleHelicopter();
-	virtual ~ModuleHelicopter();
-
-
-	bool ModuleHelicopter::Start();
-	update_status ModuleHelicopter::Update();
-};
-
+//#pragma once
+//#include "Module.h"
+//
+//struct SDL_Texture;
+//
+//class ModuleHelicopter :
+//	public Module
+//{
+//
+//public:
+//	SDL_Texture* sprites = nullptr;
+//	Animation* current_animation = nullptr;
+//
+//	
+//	SDL_Texture* helicopter_1;
+//	SDL_Texture* helicopter_2;
+//	SDL_Texture* helicopter_3;
+//
+//	Animation helicopter1;
+//	Animation helix1;
+//	Animation helix2;
+//	ModuleHelicopter();
+//	virtual ~ModuleHelicopter();
+//
+//
+//	bool ModuleHelicopter::Start();
+//	update_status ModuleHelicopter::Update();
+//};
+//
