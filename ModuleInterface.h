9ad22f1c8013a6cb1fb7fd20e841b72a0c0faf40
ninalb13 <nina.lb.x@gmail.com>
2017-04-24 10:36:29 +0200

    Starting module interface

---
+++ ModuleInterface.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Module.h"
+#include "Animation.h"
+class ModuleInterface :
+	public Module
+{
+	struct SDL_Texture;
+private:
+	SDL_Texture* graphics;
+	Animation lifes;
+
+
+public:
+	ModuleInterface();
+	virtual ~ModuleInterface();
+	bool Init();
+	bool Start();
+	update_status PreUpdate();
+	update_status Update();
+	update_status PostUpdate();
+	bool CleanUp();
+};
+
