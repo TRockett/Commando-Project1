642d10d885518526d3dd2ccb22f99a44d9b4e8bf
marc094 <marcmoreno2@gmail.com>
2017-05-04 10:25:55 +0200

    Fix: enemies no longer disapear

--- EnemyGrenade.h
+++ EnemyGrenade.h
@@ -9,16 +9,6 @@ class EnemyGrenade :
 private:
 	Path movement;
 
-	Animation e1_forward;
-	Animation e1_backward;
-	Animation e1_left;
-	Animation e1_right;
-	Animation e1_up_right;
-	Animation e1_up_left;
-	Animation e1_down_right;
-	Animation e1_down_left;
-	Animation death;
-
 	int current_angle;
 	bool collision = false;
 	bool dying = false;
