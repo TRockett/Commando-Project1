8ffc6659cc45cfc0a008ddf612f88f3e60ef95e2
DESKTOP-Marc <marcmoreno2@gmail.com>
2017-05-06 20:19:34 +0200

    Collision final fix

--- ModuleCollision.cpp
+++ ModuleCollision.cpp
@@ -230,9 +230,7 @@ bool Collider::CheckCollision(const SDL_Rect& r) const
 {
 	// TODO 0: Return true if there is an overlap
 	// between argument "r" and property "rect"
-	if (r.x <= rect.x + rect.w && r.x >= rect.x && r.y <= rect.y + rect.h && r.y >= rect.y)
-		return true;
-	if (rect.x <= r.x + r.w && rect.x >= r.x && rect.y <= r.y + r.h && rect.y >= r.y)
+	if (r.x <= rect.x + rect.w && r.x + r.w >= rect.x && r.y <= rect.y + rect.h && r.y + r.h >= rect.y)
 		return true;
 	return false;
 }
